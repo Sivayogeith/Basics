8c241cefb7e514c9f8936fa247546e5fc42c5c03
Sivayogeith <65064458+Sivayogeith@users.noreply.github.com>
2020-05-11 13:41:06 +0530

    Add files via upload

---
+++ FactorV2.java
@@ -0,0 +1,12 @@
+class Factor{
+	public static void main(String[] args){
+		int fac = 2;
+		int fac2 = 4;
+
+		if(fac * fac2 == 0) {
+			System.out.println("Yes");
+		}else{
+			System.out.println("Yes");
+		}
+	}
+}			
